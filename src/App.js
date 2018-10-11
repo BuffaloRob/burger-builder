acb340761cdd5b0972122bf71d89a0d91f85f01a
BuffaloRob <peckham.rob@gmail.com>
2018-10-11 14:04:50 -0400

    added mock-up for Layout component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Almost before we knew it</h1>
-        <p>Almost before we knew it</p>
+        <Layout>
+          <p>Test</p>
+        </Layout>
       </div>
     );
   }
