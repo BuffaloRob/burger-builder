7ebf275287cdfafd2fb7442255e6950f13c22626
BuffaloRob <peckham.rob@gmail.com>
2018-10-12 10:43:29 -0400

    hard coded BurgerIngredient components into Burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,7 +6,12 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
   return (
     <div className={classes.Burger}>
-      <BurgerIngredient/>
+      <BurgerIngredient type="bread-top"/>
+      <BurgerIngredient type="cheese" />
+      <BurgerIngredient type="meat" />
+      <BurgerIngredient type="bread-bottom" />
     </div>
   );
 };
+
+export default burger;
