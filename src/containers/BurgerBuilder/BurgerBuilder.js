40adf2a8c0c66b65dce63076a362c42727b4eff0
BuffaloRob <peckham.rob@gmail.com>
2018-10-15 10:33:46 -0400

    fixed error in how state was initially formatted in BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,11 +5,14 @@ import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
 
   state = {
-    salad: 1,
-    bacon: 1,
-    cheese: 2,
-    meat: 2,
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    },
   }
+  
   render() {
     return (
       <>
