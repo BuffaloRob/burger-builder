a79b6f113d1874bf0f37d8fb2fe49268b64847ec
BuffaloRob <peckham.rob@gmail.com>
2018-10-16 10:33:22 -0400

    + disable to 'Less' button on BuildControl. + price to BuildControls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -10,15 +10,19 @@ const controls = [
   { label: 'Cheese', type: 'cheese' },
 ];
 
-const buildControls = (props) => (
+const buildControls = props => (
   <div className={classes.BuildControls}>
+    <p>
+      Current Price: <strong>${props.price.toFixed(2)}</strong>
+    </p>
     {controls.map(ctrl => (
-      <BuildControl 
-        key={ctrl.label} 
+      <BuildControl
+        key={ctrl.label}
         label={ctrl.label}
-        added={() => props.ingredientAdded(ctrl.type)} 
+        added={() => props.ingredientAdded(ctrl.type)}
         removed={() => props.ingredientRemoved(ctrl.type)}
-        disabled={props.disabled[ctrl.type]} />
+        disabled={props.disabled[ctrl.type]}
+      />
     ))}
   </div>
 );
