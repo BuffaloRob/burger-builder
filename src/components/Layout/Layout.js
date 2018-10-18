a8403ecc275fec34fb7b565f4696a4be0b08a126
BuffaloRob <peckham.rob@gmail.com>
2018-10-18 10:19:58 -0400

    made Layout into class to maintain state for SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';   
 
-const layout = (props) => (
-  <>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </>
-)
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  }
+  
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
+  render() {
+    return (
+      <>
+        <Toolbar />
+        <SideDrawer 
+          closed={this.sideDrawerClosedHandler}
+          open={this.showSideDrawer} />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </>
+    )
+  }
+}
 
-export default layout;
+export default Layout;
