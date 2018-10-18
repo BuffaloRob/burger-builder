b6744189a373d1349723ec74d909fc01f4fb4e16
BuffaloRob <peckham.rob@gmail.com>
2018-10-18 10:44:03 -0400

    + conditional statement & props passed down from Layout

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,11 +6,14 @@ import classes from './SideDrawer.module.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
-
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open]
+  }
   return (
     <>
       <Backdrop show={props.open} clicked={props.closed}/>
-      <div className={classes.SideDrawer}>
+      <div className={attachedClasses.join(' ')}>
         <div className={classes.Logo}>
           <Logo />
         </div>
