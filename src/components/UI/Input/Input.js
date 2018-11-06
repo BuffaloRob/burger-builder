ee2e052964dfe5e5b538e9a7bff821bd6ffc783a
BuffaloRob <peckham.rob@gmail.com>
2018-11-06 17:30:13 -0500

    Input: + onChange prop for all case statements

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -10,21 +10,26 @@ const input = (props) => {
       inputElement = <input 
         className={classes.InputElement} 
         {...props.elementConfig}
-        value={props.value} />;
+        value={props.value}
+        onChange={props.change} />;
       break;
     case ('textarea'):
       inputElement = <textarea 
         className={classes.InputElement} 
         {...props.elementConfig}
-        value={props.value} />;
+        value={props.value}
+        onChange={props.change} />;
       break;
     case ('select'):
       inputElement = (
         <select
           className={classes.InputElement}
-          value={props.value}>
+          value={props.value}
+          onChange={props.change}>
           {props.elementConfig.options.map(option => (
-            <option key={option.value} value={option.value}>
+            <option 
+              key={option.value} 
+              value={option.value}>
               {option.displayValue}
             </option>
           ))}
@@ -35,7 +40,8 @@ const input = (props) => {
       inputElement = <input 
         className={classes.InputElement} 
         {...props.elementConfig}
-        value={props.value} />;
+        value={props.value}
+        onChange={props.change} />;
   }
 
   return (
