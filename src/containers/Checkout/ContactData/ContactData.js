01068064cbfb0f722682e0bbe9f075185bc83cab
BuffaloRob <peckham.rob@gmail.com>
2018-11-06 10:39:54 -0500

    ContactData: +- all <input> with <Input /> components

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders'; 
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
@@ -47,10 +48,10 @@ class ContactData extends Component {
   render() {
     let form = (
       <form>
-        <input type='text' name='name' placeholder='Your Name' className={classes.Input} />
-        <input type='email' name='email' placeholder='Your Email' className={classes.Input} />
-        <input type='text' name='street' placeholder='Your Street' className={classes.Input} />
-        <input type='text' name='postal' placeholder='Your Postal Code' className={classes.Input} />
+        <Input inputtype="input" type='text' name='name' placeholder='Your Name' />
+        <Input inputtype="input" type='email' name='email' placeholder='Your Email' />
+        <Input inputtype="input" type='text' name='street' placeholder='Your Street' />
+        <Input inputtype="input" type='text' name='postal' placeholder='Your Postal Code' />
         <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
       </form>
     );
