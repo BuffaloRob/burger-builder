bac8a873dd472ec84f24c6cffbf5f1824a3c04b7
BuffaloRob <peckham.rob@gmail.com>
2018-11-15 15:00:00 -0500

    moved actions & reducer into same named folders and updated all imports

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,7 +8,7 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import * as actionType from '../../store/actions';
+import * as actionType from '../../store/actions/actions';
 
 class BurgerBuilder extends Component {
 
