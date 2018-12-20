5024809007048f701820325a7f94d25da45e019b
buffalorob <peckham.rob@gmail.com>
2018-12-20 13:20:54 -0500

    moved  'loading' to mapStateToProps

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -93,7 +93,6 @@ class ContactData extends Component {
       }
     },
     formIsValid: false,
-    loading: false,
   };
 
   orderHandler = event => {
@@ -181,7 +180,7 @@ class ContactData extends Component {
         <Button btnType="Success" disabled={!this.state.formIsValid}>Order</Button>
       </form>
     );
-    if (this.state.loading) {
+    if (this.props.loading) {
       form = <Spinner />;
     }
     return (
@@ -196,12 +195,15 @@ class ContactData extends Component {
 const mapStateToProps = state => {
   return {
     ings: state.ingredients,
-    price: state.totalPrice
-  }
+    price: state.totalPrice,
+    loading: state.loading
+  };
 }
 
 const mapDispatchToProps = dispatch => {
-  onOrderBurger: () => dispatch(actions.purchaseBurgerStart(orderData));
+  return {
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+  };
 }
 
-export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
